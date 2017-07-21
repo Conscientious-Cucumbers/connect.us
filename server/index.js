014a123fab6a84e052c8dbb915583bdfb9b16c73
Taras Ignashchenko <tignashchenko@gmail.com>
2017-07-21 14:42:36 -0700

    modify port and host

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const app = require('./app');
 const db = require('../db');
-const PORT = process.env.port || 80 || 3000;
-const HOST = process.env.host || '138.68.42.88' || localhost;
+const PORT = process.env.port || 3000;
+const HOST = process.env.host || localhost;
 
 app.listen(PORT, HOST);
-console.log('Server running at http://${host}:${port}/');
+console.log('Server running at http://${HOST}:${PORT}/');
