6fcf9a816645aaa10a73f2a95fa021cf14c61879
Taras Ignashchenko <tignashchenko@gmail.com>
2017-07-21 14:26:04 -0700

    modified server file to include host variable and port for digital ocean

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const app = require('./app');
 const db = require('../db');
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 80 || 3000;
+const HOST = process.env.host || '138.68.42.88' || localhost;
 
-app.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(PORT, HOST);
+console.log('Server running at http://${host}:${port}/');
