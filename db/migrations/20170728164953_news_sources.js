6ec6d08dd8cdec2c926c37d2d3f8e8bb114e2bdb
pboserman <pabloboserman@gmail.com>
2017-07-30 20:22:10 -0700

    (feat) completes web_worker

--- db/migrations/20170728164953_news_sources.js
+++ db/migrations/20170728164953_news_sources.js
@@ -3,7 +3,8 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTableIfNotExists('news_sources', function(table) {
       table.increments('id').unsigned().primary();
-      table.string('source', 300).notNullable();
+      table.string('name', 300).notNullable();
+      table.string('source', 300).notNullable().unique();
     })
   ]);
 };
