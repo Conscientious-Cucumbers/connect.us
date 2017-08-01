d3d462876f149018d6b083ae9f236d9fe63d9798
Taras Ignashchenko <tignashchenko@gmail.com>
2017-08-01 14:08:21 -0700

    refactor css for profile not found page

--- client/src/components/ProfileNotExists.jsx
+++ client/src/components/ProfileNotExists.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import {
-  PageHeader,
-  Jumbotron,
-  Button
-} from 'react-bootstrap';
+import { PageHeader, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import Paper from 'material-ui/Paper';
+import FrownyFace from 'material-ui/svg-icons/social/sentiment-very-dissatisfied';
 
 const ProfileNotExists = (props) => (
-  <Jumbotron className="jumbotron">
-    <i className="fa fa-frown-o"></i>
-    <h1>Profile Not Found</h1>
-    <p>Page does not exist or may have been removed</p>
-    <p>
-      <LinkContainer to="/">
-        <Button bsStyle="primary">
+  <Paper zDepth={5}
+    className="user-does-not-exist">
+    <FrownyFace className="frowny-face"/>
+    <h1 className="profile-not-found">Profile Not Found</h1>
+    <p className="profile-not-found-message">Page does not exist or may have been removed</p>
+    <LinkContainer to="/" className="return-home-button">
+      <Button>
           Return Home
-        </Button>
-      </LinkContainer>
-    </p>
-  </Jumbotron>
+      </Button>
+    </LinkContainer>
+  </Paper>
 );
 
 export default ProfileNotExists;
