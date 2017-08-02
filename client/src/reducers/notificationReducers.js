c2beea5216f6dd96e0acebcb55df11e9042ddcfb
pboserman <pabloboserman@gmail.com>
2017-08-01 20:41:52 -0700

    (bug) fixes no image found in newsfeed for EVERYTHING

--- client/src/reducers/notificationReducers.js
+++ client/src/reducers/notificationReducers.js
@@ -2,13 +2,12 @@
 
 export const SeenNotifications = (state = null, action) => {
   switch (action.type) {
-    
+
   case 'SET_SEEN_NOTIFICATIONS':
-    console.log('**** seen notifications: ', action.payload);
     return action.payload;
   default:
     return state;
-    
+
   }
 };
 
@@ -16,7 +15,6 @@ export const UnseenNotifications = (state = null, action) => {
   switch (action.type) {
 
   case 'SET_UNSEEN_NOTIFICATIONS':
-    console.log('**** unseen notifications: ', action.payload);
     return action.payload;
   case 'FOLLOW_NOTIFICATION':
     console.log('**** unseen notifications on emit: ', [...state, action.payload]);
