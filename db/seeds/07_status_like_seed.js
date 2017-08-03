bd874295a80fcf53d1b9f548beb96919b895f8d8
Saikal <saikalk@Saikals-MacBook-Pro.local>
2017-08-03 13:47:06 -0700

    fixed liked statuses - server side

--- db/seeds/07_status_like_seed.js
+++ db/seeds/07_status_like_seed.js
@@ -9,33 +9,33 @@ exports.seed = function(knex, Promise) {
         {id_user: 1, id_status: 3},
         {id_user: 1, id_status: 1},
 
-        {id_user: 2, id_status: 1},
-        {id_user: 2, id_status: 3},
-        {id_user: 2, id_status: 2},
+        {id_user: 2, id_status: 7},
+        {id_user: 2, id_status: 8},
+        {id_user: 2, id_status: 9},
 
 
-        {id_user: 3, id_status: 2},
+        {id_user: 3, id_status: 7},
         {id_user: 3, id_status: 1},
         {id_user: 3, id_status: 3},
 
 
         {id_user: 4, id_status: 2},
         {id_user: 4, id_status: 3},
-        {id_user: 4, id_status: 1},
+        {id_user: 4, id_status: 6},
 
 
         {id_user: 5, id_status: 2},
-        {id_user: 5, id_status: 3},
+        {id_user: 5, id_status: 9},
         {id_user: 5, id_status: 1},
 
 
-        {id_user: 6, id_status: 2},
+        {id_user: 6, id_status: 12},
         {id_user: 6, id_status: 1},
         {id_user: 6, id_status: 3},
 
 
         {id_user: 7, id_status: 3},
-        {id_user: 7, id_status: 1},
+        {id_user: 7, id_status: 6},
         {id_user: 7, id_status: 2}
 
       ]);
