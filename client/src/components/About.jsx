49d482fce0d47d878ca6b4588b069f1a46ea0616
Taras Ignashchenko <tignashchenko@gmail.com>
2017-08-02 18:36:13 -0700

    css font refactoring of about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,9 +7,9 @@ const About = () => (
     className="about-entry">
     <div className="about-entry-content">
       <h1 className="about-this-page">About</h1>
-      <p className="about-page-description">Description for the app goes here</p>
+      <p className="about-page-description">ConnectHub.us is a social networking website in which users can post pictures, news, and statuses for their friends and other users to see. Users can also scroll through their news feed to view the most up-to-date news from 20+ news outlets around the world.</p>
       <div className="about-page-content">
-        Collaborators:
+        <span className="about-collaborators">Collaborators:</span>
         <ul>
           <li>Taras Ignashchenko</li>
           <li>Saikal Kalmanbetova</li>
